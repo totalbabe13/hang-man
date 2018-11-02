dc7115ba39c9fc3964dd82cd5ed4a3f4a0907f0b
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-11-02 14:47:06 -0500

    re wrote script and loading methods

--- lib/new_hangman.rb
+++ lib/new_hangman.rb
@@ -46,6 +46,9 @@ module Game_functions
  end
 
  def load_saved_game
+   system('clear')
+   puts ''
+   puts ''
    puts 'what is your PLAYER name!?'
    get_file = gets.chomp
     
@@ -60,10 +63,22 @@ module Game_functions
       	:secret_word => saved_game[:secret_word]
       }
       loaded_game
+      p saved_game
     else
       puts'GAME NOT FOUND'
     end  	
- end		
+ end
+
+ def format_loaded_game_file(game_file)
+   loaded_game = Hangman.new(game_file[:player_name],game_file[:secret_word])
+   puts '- - - - - '
+   loaded_game.game_board = game_file[:game_board]
+   loaded_game.guesses = game_file[:guesses]
+   loaded_game.letters_guessed = game_file[:letters_guessed]
+   # puts "YOU GAME HAS BEEN LOADED: #{loaded_game.player_name}"
+   loaded_game
+  
+ end
 
 end 
 
@@ -74,7 +89,7 @@ list_of_words = "wordlist.txt"
 
 class Hangman
   include Game_functions
-  attr_accessor :game_board, :guesses, :letters_guessed, :player_name, :secret_word
+  attr_accessor :player_name, :game_board, :guesses, :letters_guessed,  :secret_word
 
   def initialize(player_name,secret_word)
     @player_name = player_name
@@ -139,9 +154,62 @@ include Game_functions
         load_or_save = gets.chomp
 
         if load_or_save == '1'
+          #LOAD GAME PATH	
           user_input = true	
-          puts 'load'
+          #what is your user name? (player_name = gets.chomp)
+          loaded_game = load_saved_game 
+          current_game = format_loaded_game_file(loaded_game)
+          if current_game.game_board.length < 1
+            current_game.game_board = make_guessing_board(current_game.secret_word.join)
+          end
+             #PLAY LOADED GAME 
+            system "clear"
+            puts "YOU GAME HAS BEEN LOADED: #{current_game.player_name}" 
+            puts 'A word had been selcted for you to guess:'
+            puts ''
+            puts ''
+            puts "TYPE A LETTER IN, and THEN PRESS -->ENTER" 
+            puts ''
+            puts 'IF AT ANY TIME YOU WANT TO QUIT:: type in ---> - quit  -'
+            puts 'IF YOU WANT TO GUESS THE WORD::   type in ---> - guess -'
+            puts 'TO SAVE YOUR GAME AT ANY TIME::   type in ---> - save  -'
+            guess = gets.chomp
+          while current_game.secret_word != current_game.game_board 
+            puts 'test: MAKE A GUESS'
+            # guess = gets.chomp
+
+            if current_game.letters_guessed.include?(guess)
+               puts '         YOU\'VE GUESSED THAT LETTER ALREADY--> TRY AGAIN:'
+               guess = gets.chomp
+            elsif !current_game.letters_guessed.include?(guess)
+            	current_game.letters_guessed << guess               	   
+            end 
+
+            if current_game.secret_word.include?(guess)
+               counter = 0
+               while counter < current_game.secret_word.length
+                  if current_game.secret_word[counter]== guess
+                   	current_game.game_board[counter] = guess 
+                 end
+               end	
+            	# system('clear')
+            	    puts "#{current_game.player_name} GUESSED THE LETTER --> #{guess.upcase} "
+                    puts "THERS IS AN -> #{guess.upcase} IN THIS WORD!"
+                    puts ''
+                    puts ''
+                    puts "YOUR WORD: #{current_game.game_board}"
+                    puts ''
+                    puts "NUMBER OF GUESSES: #{current_game.guesses}"
+                    puts ''
+                    puts "LETTERS GUESSED SO FAR: #{current_game.letters_guessed}"
+            end	
+
+            # if current_game.secret_word.include?(guess)
+          end # END OF WHILE LOOP PLAY LOADED GAME 
+
+
         elsif load_or_save == '2'
+          #NEW GAME PATH	
           user_input = true
           puts 'new'
         else
@@ -165,21 +233,7 @@ include Game_functions
     end	
    end#end of responce valid loop
    		 	
-   # - - - - - - - - - - - - - - - - - -
-   ## --->IF YES :: 
-     # --> (1)LOAD SAVED GAME? enter 1
-     # --> (2)PLAY NEW GAME?   enter 2  
-
-    # - - - - - - - - - - - - - - - - - -
-     
-
-# - - - - - - - - - - - - - - - - - -
-
-
-# - - - - - - - - - - - - - - - - - -
-
-
-# - - - - - - - - - - - - - - - - - -
+  
 
 
 
